743ee1b98b5772f9ed3b714fa13bd37b27878b2b
Ahmed-Ezzat-eng <ahmedgithub777@gmail.com>
2023-04-22 10:04:54 +0200

    Update images url

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let images = [
-  '../images/bg1.jpg',
-  '../images/bg2.jpg',
-  '../images/bg3.jpg',
-  '../images/bg4.jpg',
-  '../images/bg5.jpg',
-  '../images/bg6.jpg',
+  './images/bg1.jpg',
+  './images/bg2.jpg',
+  './images/bg3.jpg',
+  './images/bg4.jpg',
+  './images/bg5.jpg',
+  './images/bg6.jpg',
 ];
 let myImage = document.querySelector('img');
 let nextBtn = document.querySelector('.nextBtn');
